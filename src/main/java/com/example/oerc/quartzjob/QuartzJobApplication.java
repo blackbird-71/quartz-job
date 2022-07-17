ebe496df99c4e0327b55c8062e722fc8007eaff3
Oscar Ruiz <blackbird3871@gmail.com>
2022-07-16 20:16:06 -0500

    Ejemplo de cron con quartz que consume un bash cada segundo (previo a funcionamiento en linux)

--- src/main/java/com/example/oerc/quartzjob/QuartzJobApplication.java
+++ src/main/java/com/example/oerc/quartzjob/QuartzJobApplication.java
@@ -6,21 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class QuartzJobApplication {
 
-	//WORKAROUND. TO BE REMOVED. Avoid problems with wrong CN of  self signed certificate
-	static {
-		javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(
-				new javax.net.ssl.HostnameVerifier(){
-
-					public boolean verify(String hostname,
-										  javax.net.ssl.SSLSession sslSession) {
-						if (hostname.equals("localhost")) {
-							return true;
-						}
-						return false;
-					}
-				});
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(QuartzJobApplication.class, args);
 	}
--- src/main/java/com/example/oerc/quartzjob/service/BillingService.java
+++ src/main/java/com/example/oerc/quartzjob/service/BillingService.java
@@ -11,6 +11,11 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import org.springframework.http.HttpHeaders;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
@@ -25,7 +30,7 @@ public class BillingService {
     public void callBillingProcess() {
 
         log.info("The sample job has begun...");
-        //getPets();
+        ejecutarJob();
         try {
             Thread.sleep(EXECUTION_TIME);
         } catch (InterruptedException e) {
@@ -40,35 +45,50 @@ public class BillingService {
         return count.get();
     }
 
-
-    @Value("${billing.endpoint}")
-    private  String ENDPOINT;
-    @Value("${billing.resource.billing}")
-    private  String URL_BILLING;
-    @Value("${billing.username}")
-    private  String USER_NAME;
-    @Value("${billing.password}")
-    private  String PASSWORD;
-    @Value("${billing.parameter.status}")
-    private String STATUS_PARAMETR;
-    @Value("${billing.iso.date.format}")
-
-    private String getPets() {
-        log.info("The gettoken Service was called...");
-        String token = "";
+    private void ejecutarJob() {
+        log.info("Ejecuta el job");
+        /*
+        Runtime runtime = Runtime.getRuntime();
         try {
-            HttpHeaders headers = new HttpHeaders();
-            headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
-            UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(ENDPOINT + URL_BILLING)
-                    .queryParam("status", STATUS_PARAMETR);
-            String fullUrl = builder.build().toString();
-            ResponseEntity<Object[]> response =  new RestTemplate().getForEntity(fullUrl,  Object[].class);
-            Object[] pets = response.getBody();
-            log.info("The getPets Service finished sucessfuly..."+pets.length);
-        } catch (RestClientException e) {
-            log.error("Error while executing callBillingProcess job", e);
+            Process p1 = runtime.exec("cmd /c C:\\Proyectos\\Oscar\\prueba.bat");
+            //Process p1 = runtime.exec("cmd /c start C:\\Proyectos\\Oscar\\prueba.bat");
+            InputStream is = p1.getInputStream();
+            int i = 0;
+            StringBuffer sb = new StringBuffer();
+            while( (i = is.read() ) != -1) {
+                sb.append((char)i);
+            }
+            log.info(sb.toString());
+        } catch(IOException ioException) {
+            log.info(ioException.getMessage());
         }
-        return token;
+        */
+        /*
+            echo "********************************************************************************"
+            echo "*                             SE EJECUTO EL PROGRAMA                           *"
+            echo "********************************************************************************"
+         */
+        try {
+            String path = "/home/refactorizando/script.sh";
+            String[] command = {"sh",path};
+            Process process = Runtime.getRuntime().exec(command);
+
+            String commandRead;
+
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            while ((commandRead = stdInput.readLine()) != null) {
+                log.info(commandRead);
+            }
+            stdInput.close();
+            BufferedReader stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+            while ((commandRead = stdError.readLine()) != null) {
+                log.info(commandRead);
+            }
+            stdInput.close();
+        } catch(IOException ioException) {
+            log.info(ioException.getMessage());
+        }
     }
 
 }
